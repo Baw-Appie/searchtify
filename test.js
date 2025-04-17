28e62fe97b6a79fe48e238230413ca9ede0a7877
thonk <hello@villainsrule.xyz>
2025-04-16 23:00:55 -0400

    show better data on test

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import Spotify from './spotify.js';
 const spotify = new Spotify();
 
 const search = await spotify.search('House of Balloons');
-console.log(search.tracksV2.items[0]);
+console.log(search.tracksV2.items[0].item.data);
 
 const search2 = await spotify.search('The Weeknd');
-console.log(search2.artists.items[0]);
+console.log(search2.artists.items[0].data);
